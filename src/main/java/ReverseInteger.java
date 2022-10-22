51e680d3d93b6ffe4bafe7e0b57deebbe542d9d3
Rhodocordax <spitzer.alex+2@gmail.com>
2022-10-22 17:26:37 -0400

    pass samples

--- src/main/java/ReverseInteger.java
+++ src/main/java/ReverseInteger.java
@@ -1,5 +1,20 @@
 public class ReverseInteger {
     public int reverse(int x) {
-        return 4;
+        boolean bool = x < 0;
+        int xToUse = (bool) ? -x : x;
+        if (bool && x == xToUse) {
+            return 0;
+        }
+
+        int len = (int) Math.log10(xToUse) + 1;
+        int reverse = 0;
+
+        for (int i = 0; i < len; i++) {
+            int divPow = (int) Math.pow(10, i);
+            int multPow = (int) Math.pow(10, len - i - 1);
+            reverse += ((xToUse / divPow) % 10) * multPow;
+        }
+
+        return (bool) ? -reverse : reverse;
     }
 }
