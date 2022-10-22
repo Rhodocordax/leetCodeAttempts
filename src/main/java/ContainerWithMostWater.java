cb1f115c6d4bff79bd7d19d7814f8a9333d23b7d
Rhodocordax <spitzer.alex+2@gmail.com>
2022-10-22 18:48:51 -0400

    pass samples and edge cases

--- src/main/java/ContainerWithMostWater.java
+++ src/main/java/ContainerWithMostWater.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        return 4;
+        int[] best = new int[2];
+        int max = Arrays.stream(height).max().getAsInt();
+
+        for (int i = 0; i < height.length; i++) {
+            for (int j = height.length - 1; j > i; j--) {
+                int containerHeight = Math.min(height[i], height[j]);
+                int area = containerHeight * (j - i);
+
+                if (area > best[0]) {
+                    best[0] = area;
+                    best[1] = containerHeight;
+                    continue;
+                }
+
+                if (best[0] / max > j - i) {
+                    break;
+                }
+            }
+        }
+
+        return best[0];
     }
 }
--- src/test/java/ContainerWithMostWaterTest.java
+++ src/test/java/ContainerWithMostWaterTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContainerWithMostWaterTest {
@@ -10,5 +12,10 @@ class ContainerWithMostWaterTest {
     void maxArea() {
         assertEquals(49, objectUnderTest.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
         assertEquals(1, objectUnderTest.maxArea(new int[]{1,1}));
+        int[] heights = new int[100000];
+        Arrays.fill(heights, 1);
+        assertEquals(99999, objectUnderTest.maxArea(heights));
+        Arrays.fill(heights, 10000);
+        assertEquals(999990000, objectUnderTest.maxArea(heights));
     }
 }
