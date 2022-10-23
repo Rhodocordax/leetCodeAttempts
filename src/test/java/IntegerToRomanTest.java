56ac6210a0355d71e0ef6f9842fc1f37b4a1105a
Rhodocordax <spitzer.alex+2@gmail.com>
2022-10-23 12:21:38 -0400

    test some edge cases

--- src/test/java/IntegerToRomanTest.java
+++ src/test/java/IntegerToRomanTest.java
@@ -11,5 +11,8 @@ class IntegerToRomanTest {
         assertEquals("III", objectUnderTest.intToRoman(3));
         assertEquals("LVIII", objectUnderTest.intToRoman(58));
         assertEquals("MCMXCIV", objectUnderTest.intToRoman(1994));
+        assertEquals("I", objectUnderTest.intToRoman(1));
+        assertEquals("X", objectUnderTest.intToRoman(10));
+        assertEquals("CI", objectUnderTest.intToRoman(101));
     }
 }
